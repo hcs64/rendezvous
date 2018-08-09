c63d1ddf036f35ca641b41264d516db539b38437
Adam Gashlin <agashlin@mozilla.com>
2018-08-09 02:15:52 -0700

    Reorganized, moved HTML to external file

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,29 @@
 extern crate hyper;
 extern crate futures;
-extern crate futures_timer;
 extern crate rand;
 
-use std::borrow::BorrowMut;
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
 use std::iter;
 use std::sync::{Arc, Mutex};
-use std::time::{Duration, Instant};
 use rand::{Rng, thread_rng};
 use rand::distributions::Alphanumeric;
-use futures::{future, stream, sync};
+use futures::{future, sync};
 use futures::{Async, Poll};
-use futures::stream::PollFn;
 use hyper::{Body, Chunk, HeaderMap, Method, Request, Response, Server, StatusCode};
 use hyper::body::Payload;
 use hyper::header::{self, HeaderValue};
 use hyper::rt::{Future, Stream};
 use hyper::service::service_fn;
-use futures_timer::Delay;
 
-// TODO: timeout doesn't work
-const TIMEOUT_SECS: u64 = 5; //5 * 60;
+// TODO: timeout
+const _TIMEOUT_SECS: u64 = 5; //5 * 60;
 
-const URL: &'static str = "http://localhost:3000/";
+static TYPE_TEXT: &'static str = "text/plain; charset=utf-8";
+static TYPE_HTML: &'static str = "text/html; charset=utf-8";
+static _URL: &'static str = "http://localhost:3000/";
+static FAVICON: &'static [u8] = include_bytes!("favicon.ico");
+static UPLOADER_HTML: &'static str = include_str!("uploader.html");
 
 enum Rendezvous {
     Bod(Body),
@@ -81,12 +80,11 @@ impl Payload for Forwarder {
         }
         self.body.take().unwrap().fuse();
         self.complete.take().unwrap().send(
-                Response::builder()
-                    .header(
-                        header::CONTENT_TYPE,
-                        HeaderValue::from_static("text/html; charset=utf-8"))
-                    .body(Bod(Body::from(r"GOTCHA SUCKAS")))
-                    .unwrap());
+            Response::builder()
+                .header(header::CONTENT_TYPE, TYPE_TEXT)
+                .body(Bod(Body::from("Upload success!")))
+                .unwrap()
+        );
         Ok(Async::Ready(None))
     }
     // TODO I needed to cheat with these because otherwise we wouldn't actually
@@ -115,186 +113,121 @@ impl Payload for Forwarder {
 type BoxFut = Box<Future<Item=Response<Rendezvous>, Error=hyper::Error> + Send>;
 type InFlightMap = Arc<Mutex<HashMap<String, Option<Forwarder>>>>;
 
-fn service(in_flight: InFlightMap) -> impl Fn(Request<Body>) -> BoxFut 
-{
-    move |req: Request<Body>| -> BoxFut {
-        let mut response = Response::new(Bod(Body::empty()));
-
-        match (req.method(), req.uri().path()) {
-            (&Method::GET, "/") => {
-                response.headers_mut().insert(
-                    header::CONTENT_TYPE,
-                    HeaderValue::from_static("text/html; charset=utf-8"));
-                *response.body_mut() = Bod(Body::from(
-r#"<!doctype html>
-<html>
-<body>
-<textarea cols=20 rows=10 id='content'></textarea>
-<br>
-<button id='submit-button'>Submit</button>
-<br>
-<div id='link-div'></div>
-<br>
-<textarea cols=20 rows=5 id='log'></textarea>
-<script>
-'use strict';
-
-let content = document.getElementById('content');
-let submit = document.getElementById('submit-button');
-let log = document.getElementById('log');
-let linkdiv = document.getElementById('link-div');
-
-let reportStatus = function (newStatus) {
-    log.value = newStatus + '\n' + log.value;
-};
-
-content.value = '';
-log.value = '';
-
-submit.addEventListener('click', function () {
-    let data = content.value;
-    let xhr = new XMLHttpRequest();
-    let token = '';
-    xhr.addEventListener('load', function () {
-        // TODO: validate token
-        token = this.responseText;
-
-        reportStatus('Got token ' + token);
-        let link = document.createElement('a');
-        link.href = 'http://localhost:3000/download?' + token;
-        link.target = '_blank';
-        link.innerText = token;
-        while (linkdiv.firstChild) {
-            linkdiv.removeChild(linkdiv.firstChild);
+macro_rules! std_response {
+    ( $t:expr, $s:expr ) => {
+        {
+            let mut response = Response::builder();
+            response.header(
+                header::CONTENT_TYPE,
+                HeaderValue::from_static($t));
+            Box::new(future::ok(response.body(Bod(Body::from($s))).unwrap()))
         }
-        linkdiv.appendChild(link);
+    }
+}
 
-        let xhr = new XMLHttpRequest();
-        xhr.addEventListener('load', function () {
-            reportStatus('On it\'s way! ' + xhr.responseText);
-        });
-        xhr.addEventListener('error', function () {
-            reportStatus('XHR error');
-        });
-        xhr.open('POST', '/upload?' + token, true);
-        xhr.send(content.value);
-        
-        reportStatus('Uploading...');
-    });
-    xhr.addEventListener('error', function () {
-        reportStatus('XHR error');
-    });
-    xhr.open('POST', '/requestToken', true);
-    xhr.send();
-    reportStatus('Requesting token');
-});
-</script>
-</body>
-</html>"#));
-            },
-            (&Method::POST, "/requestToken") => {
-                response.headers_mut().insert(
-                    header::CONTENT_TYPE,
-                    HeaderValue::from_static("text/plain; charset=utf-8"));
-                let mut rng = thread_rng();
-                let token = iter::repeat(())
-                    .map(|()| rng.sample(Alphanumeric))
-                    .take(12)
-                    .collect::<String>();
-                in_flight.lock().unwrap().insert(token.clone(), None);
-                *response.body_mut() = Bod(Body::from(token));
-            },
-            (&Method::POST, "/upload") => {
-                response.headers_mut().insert(
-                    header::CONTENT_TYPE,
-                    HeaderValue::from_static("text/plain; charset=utf-8"));
+fn service_home() -> BoxFut
+{
+    std_response!(TYPE_HTML, UPLOADER_HTML)
+}
 
-                let (parts, body) = req.into_parts();
-                let (complete, completion) = sync::oneshot::channel();
-                *response.body_mut() = Bod(Body::from(r"Thanks bud!"));
-                let query = parts.uri.query();
-                if query.is_none() {
-                    // TODO HTTP error code
-                    *response.body_mut() = Bod(Body::from(r"<b>Missing token</b>"));
-                    return Box::new(future::ok(response));
-                }
-                let token = String::from(query.unwrap());
+fn service_favicon() -> BoxFut
+{
+    std_response!("image/x-icon", FAVICON)
+}
 
-                let mut entry = match in_flight.lock().unwrap().entry(token.clone()) {
-                    Entry::Occupied(mut entry) => {
-                        entry.insert(Some(Forwarder {
-                            body: Some(body),
-                            complete: Some(complete),
-                        }))
-                    },
-                    Entry::Vacant(_) => {
-                        *response.body_mut() = Bod(Body::from(r"<b>Unknown token</b>"));
-                        return Box::new(future::ok(response));
-                    },
-                };
+fn service_request_token(in_flight: &InFlightMap) -> BoxFut
+{
+    let mut rng = thread_rng();
+    let token = iter::repeat(())
+        .map(|()| rng.sample(Alphanumeric))
+        .take(12)
+        .collect::<String>();
 
-                return Box::new(completion.or_else(|e| {
-                    future::ok(Response::new(Bod(Body::from(r"BAD NEWS"))))
-                })
-                .map_err(|_: sync::oneshot::Canceled| -> hyper::Error {
-                    unreachable!()
-                }));
-            },
-            (&Method::GET, "/download") => {
-                let query = req.uri().query();
-                if query.is_none() {
-                    response.headers_mut().insert(
-                        header::CONTENT_TYPE,
-                        HeaderValue::from_static("text/html; charset=utf-8"));
-                    *response.body_mut() = Bod(Body::from(r"<b>Token missing</b>"));
+    in_flight.lock().unwrap().insert(token.clone(), None);
 
-                    // TODO consolidate error handling
-                    return Box::new(future::ok(response));
-                }
+    std_response!(TYPE_TEXT, token)
+}
 
-                let token = String::from(query.unwrap());
-                match in_flight.lock().unwrap().remove(&token) {
-                    Some(Some(forwarder)) => {
-                        // TODO don't repeat setting headers everywhere
-                        /*
-                        response.headers_mut().insert(
-                            header::CONTENT_TYPE,
-                            HeaderValue::from_static("text/plain; charset=utf-8"));
-                        */
+fn service_upload(req: Request<Body>, in_flight: &InFlightMap) -> BoxFut
+{
+    let (parts, body) = req.into_parts();
+    let (complete, completion) = sync::oneshot::channel();
+    let query = parts.uri.query();
+    if query.is_none() {
+        // TODO HTTP error codes for errors
+        return std_response!(TYPE_TEXT, "Missing token");
+    }
+    let token = String::from(query.unwrap());
 
-                        return Box::new(future::ok(Response::builder()
-                            .body(Fwd(forwarder))
-                            .unwrap()));
-                    },
-                    Some(None) => {
-                        response.headers_mut().insert(
-                            header::CONTENT_TYPE,
-                            HeaderValue::from_static("text/html; charset=utf-8"));
-                        *response.body_mut() = Bod(Body::from(r"<b>Uploader never connected with token</b>"));
-                    },
-                    None => {
-                        response.headers_mut().insert(
-                            header::CONTENT_TYPE,
-                            HeaderValue::from_static("text/html; charset=utf-8"));
-                        *response.body_mut() = Bod(Body::from(r"<b>Bad token</b>"));
-                    },
-                };
-            },
-            _ => {
-                response.headers_mut().insert(
-                    header::CONTENT_TYPE,
-                    HeaderValue::from_static("text/html; charset=utf-8"));
-                *response.status_mut() = StatusCode::NOT_FOUND;
-                *response.body_mut() = Bod(Body::from(r"<b>404 Not Found</b>"));
-            }
-        };
+    match in_flight.lock().unwrap().entry(token.clone()) {
+        Entry::Occupied(mut entry) => {
+            entry.insert(Some(Forwarder {
+                body: Some(body),
+                complete: Some(complete),
+            }))
+        },
+        Entry::Vacant(_) => {
+            return std_response!(TYPE_TEXT, "Unknown token");
+        },
+    };
 
-        Box::new(future::ok(response))
+    Box::new(completion.or_else(|_|
+        future::ok(
+            Response::builder()
+                .header(header::CONTENT_TYPE, TYPE_TEXT)
+                .body(Bod(Body::from("BAD NEWS")))
+                .unwrap())
+    )
+    .map_err(|_: sync::oneshot::Canceled| -> hyper::Error {
+        unreachable!()
+    }))
+}
+
+fn service_download(req: Request<Body>, in_flight: &InFlightMap) -> BoxFut
+{
+    let query = req.uri().query();
+    if query.is_none() {
+        // TODO consolidate error handling
+        return std_response!(TYPE_HTML, r"<b>Token missing</b>");
+    }
+
+    let token = String::from(query.unwrap());
+    match in_flight.lock().unwrap().remove(&token) {
+        Some(Some(forwarder)) => {
+            Box::new(future::ok(
+                    Response::builder()
+                    .body(Fwd(forwarder))
+                    .unwrap()))
+        },
+        Some(None) =>  std_response!(TYPE_HTML, "<b>Uploader never connected for upload</b>"),
+        None => std_response!(TYPE_HTML, "<b>Unknown token</b>"),
+    }
+}
+
+fn service_not_found() -> BoxFut
+{
+    let mut response = Response::builder();
+
+    response.header(header::CONTENT_TYPE, HeaderValue::from_static(TYPE_HTML));
+    response.status(StatusCode::NOT_FOUND);
+
+    Box::new(future::ok(response.body(Bod(Body::from(r"<b>404 Not Found</b>"))).unwrap()))
+}
+
+fn service(in_flight: InFlightMap) -> impl Fn(Request<Body>) -> BoxFut
+{
+    move |req| match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => service_home(),
+        (&Method::GET, "/favicon.ico") => service_favicon(),
+        (&Method::POST, "/requestToken") => service_request_token(&in_flight),
+        (&Method::POST, "/upload") => service_upload(req, &in_flight),
+        (&Method::GET, "/download") => service_download(req, &in_flight),
+        _ => service_not_found(),
     }
 }
 
 fn main() {
-    let addr = ([127, 0, 0, 1], 3000).into();
+    let addr = ([0, 0, 0, 0], 3000).into();
 
     let in_flight = Arc::new(Mutex::new(HashMap::new()));
 
